6165b108b6b915e904f75a089d83cda218a8782a
Pranshu Patel <pranshu05patel@gmail.com>
2023-09-14 20:28:51 +0530

    Prettier :sparkles:

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,32 +1,32 @@
-const { createInteractionLogEmbed } = require('../utils/intlogemb');
+const { createInteractionLogEmbed } = require("../utils/intlogemb")
 
 module.exports = {
-  name: 'interactionCreate',
+  name: "interactionCreate",
   async execute(interaction) {
     if (!interaction.isCommand() || !interaction.guild) {
-      return interaction.reply('You can use slash commands only in a server!');
+      return interaction.reply("You can use slash commands only in a server!")
     }
 
-    const command = interaction.client.commands.get(interaction.commandName);
+    const command = interaction.client.commands.get(interaction.commandName)
 
-    if (!command) return;
+    if (!command) return
 
     try {
-      await command.execute(interaction);
+      await command.execute(interaction)
 
-      const channelId = '1151789513883013130';
-      const channel = interaction.guild.channels.cache.get(channelId);
+      const channelId = "1151789513883013130"
+      const channel = interaction.guild.channels.cache.get(channelId)
 
       if (channel) {
-        const interactionLogEmbed = createInteractionLogEmbed(interaction);
-        channel.send({ embeds: [interactionLogEmbed] });
+        const interactionLogEmbed = createInteractionLogEmbed(interaction)
+        channel.send({ embeds: [interactionLogEmbed] })
       }
     } catch (err) {
-      console.error(err);
+      console.error(err)
       await interaction.reply({
-        content: 'Some error occurred!',
+        content: "Some error occurred!",
         ephemeral: true,
-      });
+      })
     }
   },
-};
+}
