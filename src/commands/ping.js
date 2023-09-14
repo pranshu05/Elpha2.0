ec36503c4e327bbe3890a544b22b2e9ba400d650
Pranshu Patel <pranshu05patel@gmail.com>
2023-09-14 18:34:58 +0530

    Installed nodemon and created interaction logs :rocket:

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,10 +1,20 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { EmbedBuilder } = require('discord.js')
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-        await interaction.reply('Pong!')
-	},
-};
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Pong!'),
+    async execute(interaction) {
+        const embed = new EmbedBuilder()
+        .setColor('#00ffff')
+        .setTimestamp()
+        .setTitle(`Pong 🏓`)
+        .addFields(
+            {name: 'Latancy:', value: `\`\`\`${(interaction.client.ws.ping).toFixed(0)} ms\`\`\``, inline: true},
+            {name: 'API Latancy:', value: `\`\`\`${Date.now() - interaction.createdTimestamp} ms\`\`\``, inline: true}
+          )
+        .setFooter({text: `counted for ${interaction.user.globalName}`})
+        interaction.reply({ embeds: [embed] })
+    }
+}
